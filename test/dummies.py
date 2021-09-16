4d7c9595f45f5ed6432d07afa576203c1bdea365
Alexandre Mahdhaoui <alexandre.mahdhaoui@gmail.com>
2021-09-16 12:31:31 +0200

    reformating tests

--- test/dummies.py
+++ test/dummies.py
@@ -1,2 +1,18 @@
+import os
+
+import dotenv
+from pymongo import MongoClient
+
+dotenv.load_dotenv('.env.test.mongo_db')
+
+
 class DummyBaseAdapter:
-    pass
+    _namespace = {
+        'fromage': MongoClient(os.getenv("CNX_STR"))[os.getenv("DB_NAME")]['collection_{}'.format('fromage')]
+    }
+
+    def __class_getitem__(cls, item: str) -> MongoClient:
+        return cls._namespace[item]
+
+    def __getitem__(self, item) -> MongoClient:
+        return self._namespace[item]
--- test/operations/query/test_query.py
+++ test/operations/query/test_query.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 import pytest
 from devtools import debug
@@ -7,6 +6,8 @@ from pymongo import MongoClient
 
 from better_graph.operations.query.query import Query
 from test.dummies import DummyBaseAdapter
+
+
 #  TODO: !!RESOLVED!!
 #   Please find a solution to Pydantic's _id problem
 #       Either don't use Pydantic at all
@@ -15,23 +16,25 @@ from test.dummies import DummyBaseAdapter
 #               bson.objectid.ObjectId(_id).__str__() to get _id as a string
 
 class QueryTest:
+
+    def test_query(self):
+        test = [t.dict() for t in self.query(self.test)]
+        assertion = [self.assertion]
+        debug(test, assertion)
+        assert (test == assertion)
+
     @pytest.fixture
     def fixture(self):
         self.setup()
         yield
         self.teardown()
 
-    def test_query(self):
-        test = [t.dict() for t in self.query(self.test)]
-        assertion = [self.assertion]
-        debug(test, assertion)
-        assert(test == assertion)
-
     def setup(self) -> None:
         with open('better_graph/operations/query/test/test_query.json') as file:
             data = json.load(file)
+            name: str = data['query_model']['name']
             self.query = Query(
-                name=data['query_model']['name'],
+                name=name,
                 fields=data['query_model']['fields'],
                 base_adapter=DummyBaseAdapter,
                 excluded_query_params=data['query_model']['excluded_query_params'],
@@ -42,10 +45,8 @@ class QueryTest:
             self.assertion = data['assertion']
             self.documents = data['documents']
 
-            cnx_str = os.getenv("CNX_STR")
-            db_name = os.getenv("DB_NAME")
-            self.collection = MongoClient(cnx_str)[db_name]['collection_{}'.format('fromage')]
-            self.collection.insert_many(self.documents)
+            self.adapter: MongoClient = DummyBaseAdapter()[name]
+            self.adapter.insert_many(self.documents)
 
     def teardown(self) -> None:
-        self.collection.drop()
+        self.adapter.drop()
